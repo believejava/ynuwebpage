a8f4a439a617b26776cf52f78d3a0a2848cd3338
黄玮鹏 <chinawphuang@163.com>
2017-02-15 16:02:34 +0800

    comment invalid code out
    remove notice roll effect

--- js/public.js
+++ js/public.js
@@ -10,11 +10,13 @@ $(function(){
 	//导航栏
 	navFuc();	
 	//通知公告
-	if($("#demo")[0] && $("#demo1")[0] && $("#demo2")[0]){
-	noteScroll();}	
+	// if($("#demo")[0] && $("#demo1")[0] && $("#demo2")[0]){
+	//    noteScroll();
+    // }	
 	//tab切换
 	slideTab();
 })
+
 //站内搜索
 function searchFuc(){
 	$("#searchTxt").focus(function(){
@@ -32,6 +34,7 @@ function searchFuc(){
 		}
 	})
 }
+
 //导航栏
 function navFuc(){
 	if($("#mainNav")){
@@ -45,26 +48,28 @@ function navFuc(){
 		)	
 	}
 }
+
 //通知公告
-function noteScroll(){
-	var demo = document.getElementById("demo");
-	var demo1 = document.getElementById("demo1");
-	var demo2 = document.getElementById("demo2");
-	var speed = 36000000;    //滚动速度值，值越大速度越慢
-	var nnn=200/demo1.offsetHeight;
-	for(var i=0;i<nnn;i++){demo1.innerHTML+=demo1.innerHTML}
-	demo2.innerHTML = demo1.innerHTML    //克隆demo2为demo1
-	function Marquee(){
-		if(demo2.offsetTop-demo.scrollTop<=0)    //当滚动至demo1与demo2交界时
-			demo.scrollTop-=demo1.offsetHeight    //demo跳到最顶端
-		else{
-			demo.scrollTop++
-		}
-	}
-	var MyMar = setInterval(Marquee,speed);        //设置定时器
-	demo.onmouseover = function(){clearInterval(MyMar)}    //鼠标经过时清除定时器达到滚动停止的目的
-	demo.onmouseout = function(){MyMar = setInterval(Marquee,speed)}    //鼠标移开时重设定时器	
-}
+// function noteScroll(){
+// 	var demo = $("#demo")[0];
+// 	var demo1 = $("#demo1")[0];
+// 	var demo2 = $("#demo2")[0];
+// 	var speed = 150;    //滚动速度值，值越大速度越慢
+// 	var nnn=200/demo1.offsetHeight;
+// 	for(var i=0;i<nnn;i++){demo1.innerHTML+=demo1.innerHTML}
+// 	demo2.innerHTML = demo1.innerHTML    //克隆demo2为demo1
+// 	function Marquee(){
+// 		if(demo2.offsetTop-demo.scrollTop<=0)    //当滚动至demo1与demo2交界时
+// 			demo.scrollTop-=demo1.offsetHeight    //demo跳到最顶端
+// 		else{
+// 			demo.scrollTop++
+// 		}
+// 	}
+// 	var MyMar = setInterval(Marquee,speed);        //设置定时器
+// 	demo.onmouseover = function(){clearInterval(MyMar)}    //鼠标经过时清除定时器达到滚动停止的目的
+// 	demo.onmouseout = function(){MyMar = setInterval(Marquee,speed)}    //鼠标移开时重设定时器	
+// }
+
 //tab切换
 function slideTab(){
 	$("#tabList li").mouseover(function(){//如要点击切换，把mouseover改为click
